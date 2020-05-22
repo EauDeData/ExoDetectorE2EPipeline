8b8baa16062e618f9b88566377ad14a77d2b0b51
IararIV <64100950+IararIV@users.noreply.github.com>
2020-05-22 16:01:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ Created on Thu May 21 16:41:26 2020
 
 @author: gerard
 """
+import skyCleaner, skyAlignment, skyTracker, skyBrian
+import matplotlib.pyplot as plt
+import numpy as np
 
-def get_params(obs, df, ff, fdf):
-    
+obs = df = ff = fdf = './cleanObservations/'
 
-def main():
-    pass
+images, df, ff, fdf = skyCleaner.loadImages(obs, df, ff, fdf)
+#cleanImages = skyCleaner.cleanImages(images, df, ff, fdf)
+cleanImages = images
+alignedImages = skyAlignment.cross_correlation_fourier(cleanImages)
+starCoords , viewCoords = skyTracker.getStarCoords(images[0])
+print("Getting curves...")
+lightCurve = skyBrian.getBrians(images, starCoords)
+lightCurves = skyBrian.getBetterBrians(lightCurve)
+print("Done!")
     
     
